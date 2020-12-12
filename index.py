f1960134a0752c0a6f176f117dee1b821d80ded5
jad el mir <jadelmir>
2020-12-11 22:51:29 -0500

    comments

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ import xml.etree.ElementTree as ET
 
 def fetchXmlData():
     url = "https://www.senate.gov/general/contact_information/senators_cfm.xml"
-    # for some reason
+    # for fake a browser visit 
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36', }
     response = requests.get(url, headers=headers)
     return response.content
